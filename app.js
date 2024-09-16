a523fa553cc3c7c3e2fecb10a723a1bb26377e72
paulopointevin17 <paulopointevin17@yahoo.com.br>
2024-09-16 20:29:57 -0300

    Voltando para 110

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 alert('Seja Bem vindo ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 150 + 1);
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 150');
+    chute = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
