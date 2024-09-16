500346e2c6723848ec8d8ef14f030439aa772124
paulopointevin17 <paulopointevin17@yahoo.com.br>
2024-09-16 19:30:16 -0300

    alterando repositorio local

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 alert('Seja Bem vindo ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 50 + 1);
+let numeroSecreto = parseInt(Math.random() * 40 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 50');
+    chute = prompt('Escolha um número entre 1 e 40');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
