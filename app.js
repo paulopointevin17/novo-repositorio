efffefb62c533ba0a69b8dbf41016a5d5ec76ae6
paulopointevin17 <paulopointevin17@yahoo.com.br>
2024-09-16 20:30:25 -0300

    Alterando o limite máximo para 50

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 alert('Seja Bem vindo ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroSecreto = parseInt(Math.random() * 50 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt('Escolha um número entre 1 e 50');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
         break;
